a5d078e051e7789901f0034a0dcb0f3e9bbef9e4
soheil najafi <soheilnajafimail2@gmail.com>
2019-11-03 21:08:37 +0330

    add password to interface

--- src/models/interfaces/link.interface.ts
+++ src/models/interfaces/link.interface.ts
@@ -2,12 +2,13 @@ import { AnalyticsData } from "./analytics-data.interface";
 import { Document } from "mongoose";
 
 export interface ILink extends Document {
-  _id: string;
-  userId?: string, //this attribute will be added in next branch.
-  // categoryId?: string, this attribute will be added in next branch.
-  address: string;
-  shorten: string;
-  createDate: Date;
-  data: AnalyticsData[];
-  createDateFa: any;
+   _id: string;
+   userId?: string; //this attribute will be added in next branch.
+   // categoryId?: string, this attribute will be added in next branch.
+   address: string;
+   shorten: string;
+   createDate: Date;
+   data: AnalyticsData[];
+   createDateFa: any;
+   password: string;
 }

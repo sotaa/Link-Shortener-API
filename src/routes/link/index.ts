687c7642ed87ca48500a4cbf19e5e10ef39eca58
soheil najafi <soheilnajafimail2@gmail.com>
2019-11-26 15:42:44 +0330

    binding this in route

--- src/routes/link/index.ts
+++ src/routes/link/index.ts
@@ -1,36 +1,42 @@
 import { LinkPaths } from "./link.paths";
 import * as express from "express";
-import LinksController from "./controllers/links.controller";
+import { LinksController } from "./controllers/links.controller";
 import { CategoryController } from "./controllers/category.controller";
 
 class LinkRouter {
-   private controller: LinksController;
-   private categoryController: CategoryController;
+  private controller: LinksController;
+  private categoryController: CategoryController;
 
-   router: express.Router;
+  router: express.Router;
 
-   constructor() {
-      this.controller = new LinksController();
-      this.categoryController = new CategoryController();
-      this.router = express.Router();
-      this.init();
-   }
+  constructor() {
+    this.controller = new LinksController();
+    this.categoryController = new CategoryController();
+    this.router = express.Router();
+    this.init();
+  }
 
-   // initial the routes link managements.
-   private init() {
-      this.router.route(LinkPaths.root).post(this.controller.create);
-      this.router.route(LinkPaths.singleLink).put(this.controller.update);
-      this.router.route(LinkPaths.root).get(this.controller.getUserLinks);
-      this.router.route(LinkPaths.singleLink).get(this.controller.getUserLink);
-      this.router
-         .route(LinkPaths.singleLink)
-         .delete(this.controller.deleteUserLink);
-      // init category actions.
-      this.router
-         .route(LinkPaths.category)
-         .post(this.categoryController.create);
-      this.router.route(LinkPaths.category).get(this.categoryController.get);
-   }
+  // initial the routes link managements.
+  private init() {
+    this.router
+      .route(LinkPaths.root)
+      .post(this.controller.create.bind(this.controller));
+    this.router
+      .route(LinkPaths.singleLink)
+      .put(this.controller.update.bind(this.controller));
+    this.router.route(LinkPaths.root).get(this.controller.getUserLinks);
+    this.router
+      .route(LinkPaths.singleLink)
+      .delete(this.controller.deleteUserLink);
+    // init category actions.
+    this.router
+      .route(LinkPaths.category)
+      .post(this.categoryController.create.bind(this.categoryController));
+    this.router
+      .route(LinkPaths.category)
+      .get(this.categoryController.get.bind(this.categoryController));
+    this.router.route(LinkPaths.singleLink).get(this.controller.getUserLink);
+  }
 }
 
 export default new LinkRouter().router;

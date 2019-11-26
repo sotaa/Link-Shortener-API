b49309d66179ba4e8ea082b79ebad7b36a18a98a
soheil najafi <soheilnajafimail2@gmail.com>
2019-11-26 15:41:56 +0330

    add category property to link interface

--- src/models/interfaces/link.interface.ts
+++ src/models/interfaces/link.interface.ts
@@ -2,14 +2,15 @@ import { AnalyticsData } from "./analytics-data.interface";
 import { Document } from "mongoose";
 
 export interface ILink extends Document {
-   _id: string;
-   userId?: string; //this attribute will be added in next branch.
-   // categoryId?: string, this attribute will be added in next branch.
-   address: string;
-   shorten: string;
-   createDate: Date;
-   data: AnalyticsData[];
-   createDateFa: any;
-   password: string;
-   private: boolean;
+  _id: string;
+  userId?: string; //this attribute will be added in next branch.
+  // categoryId?: string, this attribute will be added in next branch.
+  address: string;
+  shorten: string;
+  createDate: Date;
+  data: AnalyticsData[];
+  createDateFa: any;
+  password: string;
+  private: boolean;
+  categories: string;
 }

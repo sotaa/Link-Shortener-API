306c68c4d2ebe795c11f32b2f1aa46235869f303
soheil najafi <soheilnajafimail2@gmail.com>
2019-11-26 15:40:58 +0330

    add category property to link schema

--- src/models/entities/link.schema.ts
+++ src/models/entities/link.schema.ts
@@ -1,6 +1,6 @@
 import { ILink } from "./../interfaces/link.interface";
 import { AnalyticsData } from "./../interfaces/analytics-data.interface";
-import { Schema, Model, model } from "mongoose";
+import { Schema, Model, model, Types } from "mongoose";
 import Messages from "../../preferences/Messages";
 import shortenLinkGenerator from "../../business-logic/link/shorten-link-generator";
 import { ObjectID } from "bson";
@@ -8,50 +8,51 @@ import { isURL } from "validator";
 const PersianDate = require("persian-date");
 
 export const LinkSchema = new Schema({
-   address: {
-      type: String,
-      validate: [isURL, Messages.linkMessages.linkIsNotCorrect],
-      required: [true, Messages.linkMessages.linkIsRequired]
-   },
-   userId: {
-      type: String,
-      required: false
-   },
-   shorten: {
-      type: String,
-      unique: true,
-      required: Messages.linkMessages.shortenIsRequired,
-      default: shortenLinkGenerator.generate
-   },
-   createDate: {
-      type: Date,
-      required: Messages.linkMessages.createDateIsRequired,
-      default: Date.now
-   },
-   createDateFa: {
-      type: Object,
-      required: Messages.linkMessages.createDateIsRequired,
-      default: new PersianDate()
-   },
-   data: {
-      type: Array<AnalyticsData>(),
-      default: []
-   },
-   password: {
-      type: String,
-      min:3
-   },
-   private: {
-      type: Boolean
-   }
+  address: {
+    type: String,
+    validate: [isURL, Messages.linkMessages.linkIsNotCorrect],
+    required: [true, Messages.linkMessages.linkIsRequired]
+  },
+  userId: {
+    type: String,
+    required: false
+  },
+  categories: [String],
+  shorten: {
+    type: String,
+    unique: true,
+    required: Messages.linkMessages.shortenIsRequired,
+    default: shortenLinkGenerator.generate
+  },
+  createDate: {
+    type: Date,
+    required: Messages.linkMessages.createDateIsRequired,
+    default: Date.now
+  },
+  createDateFa: {
+    type: Object,
+    required: Messages.linkMessages.createDateIsRequired,
+    default: new PersianDate()
+  },
+  data: {
+    type: Array<AnalyticsData>(),
+    default: []
+  },
+  password: {
+    type: String,
+    min: 3
+  },
+  private: {
+    type: Boolean
+  }
 });
 
 // add user data to a link data.
 LinkSchema.methods.addUserData = (userData: AnalyticsData, link: ILink) => {
-   return Link.updateOne(
-      { _id: new ObjectID(link._id) },
-      { $push: { data: userData } }
-   );
+  return Link.updateOne(
+    { _id: new ObjectID(link._id) },
+    { $push: { data: userData } }
+  );
 };
 
 export const Link: Model<ILink> = model<ILink>("Link", LinkSchema);

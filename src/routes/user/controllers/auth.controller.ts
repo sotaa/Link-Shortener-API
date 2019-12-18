c7aaaf85eb8b396d7b3723b42e22c549e2d2326a
soheil najafi <soheilnajafimail2@gmail.com>
2019-12-18 12:44:29 +0330

    fix error message for registering

--- src/routes/user/controllers/auth.controller.ts
+++ src/routes/user/controllers/auth.controller.ts
@@ -5,7 +5,6 @@ import Messages from "../../../preferences/Messages";
 import { IUser } from "../../../models/interfaces/user.interface";
 
 export class AuthController {
-
   // register new user.
   register(req: Request, res: Response) {
     const data = _.pick(req.body, ["email", "password", "name"]);
@@ -17,30 +16,32 @@ export class AuthController {
       .then(() => {
         // generate the token.
         user.generateAuthToken().then((token: string) => {
-          res.header("x-new-token",'Bearer ' +  token).send(user.toJSON());
+          res.header("x-new-token", "Bearer " + token).send(user.toJSON());
         });
       })
       .catch(err => {
-        res.status(400).send({message: err.message});
+        res.status(400).send(err.message);
       });
   }
 
   // login requests handler.
-  login(req: Request , res: Response) {
-    const data = _.pick(req.body , ["email" , "password"]);
-    User.schema.statics.findByCredentials(data).then((user: IUser) => {
-      if(!user) {
-        res.status(404).send({message: Messages.userMessages.loginFailed});
-        return;
-      }
+  login(req: Request, res: Response) {
+    const data = _.pick(req.body, ["email", "password"]);
+    User.schema.statics
+      .findByCredentials(data)
+      .then((user: IUser) => {
+        if (!user) {
+          res.status(404).send({ message: Messages.userMessages.loginFailed });
+          return;
+        }
 
-      // generate new token for logged in user.
-      user.generateAuthToken().then(token => {
-        res.header('x-new-Token' , 'Bearer ' + token).send(user.toJSON());
+        // generate new token for logged in user.
+        user.generateAuthToken().then(token => {
+          res.header("x-new-Token", "Bearer " + token).send(user.toJSON());
+        });
+      })
+      .catch((err: any) => {
+        res.status(400).send(err);
       });
-      
-    }).catch((err: any) => {
-      res.status(400).send(err);
-    });
   }
 }
